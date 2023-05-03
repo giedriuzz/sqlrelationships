ded80119f28990f5d246efd0b805cb06e99bcc36
kraujinis <kraujinis@gmail.com>
2023-05-03 19:20:51 +0300

    change file alocation add for_tests folder

--- old_sessions.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.tasks import Task
-from models.user import User
-from db.base import Base
-
-engine = create_engine("sqlite:///tasks_new.db")
-Session = sessionmaker(bind=engine)
-session = Session()
